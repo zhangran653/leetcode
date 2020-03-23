f1dc23fd71cea81f13c438a84fc8bc9f689ea258
zhangran <zhangran@42.com>
2020-03-23 15:19:36 +0800

    leetcode

---
+++ src/leetcode/editor/cn/[20]有效的括号.java
@@ -0,0 +1,68 @@
+package leetcode.editor.cn;//给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断字符串是否有效。
+//
+// 有效字符串需满足： 
+//
+// 
+// 左括号必须用相同类型的右括号闭合。 
+// 左括号必须以正确的顺序闭合。 
+// 
+//
+// 注意空字符串可被认为是有效字符串。 
+//
+// 示例 1: 
+//
+// 输入: "()"
+//输出: true
+// 
+//
+// 示例 2: 
+//
+// 输入: "()[]{}"
+//输出: true
+// 
+//
+// 示例 3: 
+//
+// 输入: "(]"
+//输出: false
+// 
+//
+// 示例 4: 
+//
+// 输入: "([)]"
+//输出: false
+// 
+//
+// 示例 5: 
+//
+// 输入: "{[]}"
+//输出: true 
+// Related Topics 栈 字符串
+
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution20 {
+    public boolean isValid(String s) {
+        Deque<Character> stack = new ArrayDeque<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '[' || c == '{') {
+                stack.push(c);
+            } else if (stack.isEmpty()) {
+                return false;
+            } else if (c == ')' && stack.pop() != '(') {
+                return false;
+            } else if (c == ']' && stack.pop() != '[') {
+                return false;
+            } else if (c == '}' && stack.pop() != '{') {
+                return false;
+            }
+
+        }
+        return stack.isEmpty();
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
