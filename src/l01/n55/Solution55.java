5961e394724e59015456e62efd7ba60015a2a861
zhangran <zhangran@42.com>
2020-04-09 11:36:39 +0800

    leetcode

--- src/l01/n55/Solution55.java
+++ src/l01/n55/Solution55.java
@@ -34,5 +34,28 @@ class Solution55 {
         }
         return right == 0;
     }
+
+
+    /**
+     * 如果某一个作为 起跳点 的格子可以跳跃的距离是 3，那么表示后面 3 个格子都可以作为 起跳点。
+     * 可以对每一个能作为 起跳点 的格子都尝试跳一次，把 能跳到最远的距离 不断更新。
+     * 如果可以一直跳到最后，就成功了。
+     * <p>
+     * 作者：ikaruga
+     * 链接：https://leetcode-cn.com/problems/jump-game/solution/55-by-ikaruga/
+     *
+     * @param nums
+     * @return
+     */
+    public boolean canJump1(int[] nums) {
+        int k = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (i > k) {
+                return false;
+            }
+            k += Math.max(k, i + nums[i]);
+        }
+        return true;
+    }
 }
 //leetcode submit region end(Prohibit modification and deletion)
