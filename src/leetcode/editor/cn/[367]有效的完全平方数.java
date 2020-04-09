5e6bee6c42c4b537b9442a02b16c380597367990
zhangran <zhangran@42.com>
2020-04-09 15:19:22 +0800

    367

---
+++ src/leetcode/editor/cn/[367]有效的完全平方数.java
@@ -0,0 +1,38 @@
+package leetcode.editor.cn;//给定一个正整数 num，编写一个函数，如果 num 是一个完全平方数，则返回 True，否则返回 False。
+//
+// 说明：不要使用任何内置的库函数，如 sqrt。 
+//
+// 示例 1： 
+//
+// 输入：16
+//输出：True 
+//
+// 示例 2： 
+//
+// 输入：14
+//输出：False
+// 
+// Related Topics 数学 二分查找
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution367 {
+    public boolean isPerfectSquare(int num) {
+        if (num < 2) {
+            return true;
+        }
+        long left = 1, right = num, mid = 1;
+        while (left <= right) {
+            mid = left + (right - left) / 2;
+            if (mid * mid == num) {
+                return true;
+            } else if (mid * mid < num) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return false;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
