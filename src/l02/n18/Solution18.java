cd22261cc902244b444f444264c6ab8b0970c2a4
zhangran <zhangran@42.com>
2020-04-12 16:33:52 +0800

    leetcode

---
+++ src/l02/n18/Solution18.java
@@ -0,0 +1,79 @@
+package l02.n18;//给定一个包含 n 个整数的数组 nums 和一个目标值 target，判断 nums 中是否存在四个元素 a，b，c 和 d ，使得 a + b + c +
+// d 的值与 target 相等？找出所有满足条件且不重复的四元组。 
+//
+// 注意： 
+//
+// 答案中不可以包含重复的四元组。 
+//
+// 示例： 
+//
+// 给定数组 nums = [1, 0, -1, 0, -2, 2]，和 target = 0。
+//
+//满足要求的四元组集合为：
+//[
+//  [-1,  0, 0, 1],
+//  [-2, -1, 1, 2],
+//  [-2,  0, 0, 2]
+//]
+// 
+// Related Topics 数组 哈希表 双指针
+
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution18 {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        List<List<Integer>> res = new LinkedList<>();
+        if (nums == null || nums.length < 4) {
+            return res;
+        }
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 2; i++) {
+
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            for (int j = i + 1; j < nums.length - 1; j++) {
+
+                if (j > i + 1 && nums[j] == nums[j - 1]) {
+                    continue;
+                }
+                int l = j + 1;
+                int r = nums.length - 1;
+                while (l < r) {
+                    int sum = nums[i] + nums[j] + nums[l] + nums[r];
+                    if (sum == target) {
+                        res.add(Arrays.asList(nums[i], nums[j], nums[l], nums[r]));
+                        l++;
+                        r--;
+                        while (l < r && nums[l] == nums[l - 1]) {
+                            l++;
+                        }
+
+                        while (l < r && nums[r] == nums[r + 1]) {
+                            r--;
+                        }
+                    } else if (sum < target) {
+                        l++;
+                        while (l < r && nums[l] == nums[l - 1]) {
+                            l++;
+                        }
+                    } else {
+                        r--;
+                        while (l < r && nums[r] == nums[r + 1]) {
+                            r--;
+                        }
+                    }
+                }
+            }
+        }
+        return res;
+    }
+
+
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
---
+++ src/l03/n15/Solution15.java
@@ -0,0 +1,83 @@
+package l03.n15;//给你一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？请你找出所有满足条件且不重复
+//的三元组。 
+//
+// 注意：答案中不可以包含重复的三元组。 
+//
+// 
+//
+// 示例： 
+//
+// 给定数组 nums = [-1, 0, 1, 2, -1, -4]，
+//
+//满足要求的三元组集合为：
+//[
+//  [-1, 0, 1],
+//  [-1, -1, 2]
+//]
+// 
+// Related Topics 数组 双指针
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+/**
+ * 哈希表
+ */
+class Solution15 {
+
+
+    /**
+     * 排序+双指针
+     *
+     * @param nums
+     * @return
+     */
+    public List<List<Integer>> threeSum2(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (nums == null || nums.length < 3) {
+            return res;
+        }
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (nums[i] > 0) {
+                return res;
+            }
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            int l = i + 1;
+            int r = nums.length - 1;
+            while (l < r) {
+                int sum = nums[i] + nums[l] + nums[r];
+                if (sum == 0) {
+                    res.add(Arrays.asList(nums[i], nums[l], nums[r]));
+                    l++;
+                    r--;
+                    while (l < r && nums[l] == nums[l - 1]) {
+                        l++;
+                    }
+                    while (l < r && nums[r] == nums[r + 1]) {
+                        r--;
+                    }
+                } else if (sum > 0) {
+                    l++;
+                    while (l < r && nums[l] == nums[l - 1]) {
+                        l++;
+                    }
+                } else {
+                    r--;
+                    while (l < r && nums[r] == nums[r + 1]) {
+                        r--;
+                    }
+                }
+            }
+        }
+
+        return res;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
