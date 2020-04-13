6b0c59c7bcbd02a5f4a585aab7b18d282c251ddc
zhangran <zhangran@42.com>
2020-04-13 10:26:03 +0800

    leetcode

--- src/l02/n141/Solution141.java
+++ src/l02/n141/Solution141.java
@@ -1,4 +1,4 @@
-package l01.n141;//给定一个链表，判断链表中是否有环。
+package l02.n141;//给定一个链表，判断链表中是否有环。
 //
 // 为了表示给定链表中的环，我们使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。 如果 pos 是 -1，则在该链表中没有环。 
 //
@@ -64,19 +64,16 @@ class Solution141 {
     }
 
     public boolean hasCycle(ListNode head) {
-        if (head == null || head.next == null) {
-            return false;
-        }
         ListNode fast = head;
         ListNode slow = head;
         while (fast != null && fast.next != null) {
             fast = fast.next.next;
             slow = slow.next;
-            if (slow == fast) {
+            if (fast == slow) {
                 return true;
             }
         }
-        return true;
+        return false;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
--- src/l02/n142/Solution142.java
+++ src/l02/n142/Solution142.java
@@ -1,13 +1,15 @@
-package l01.n141;//给定一个链表，判断链表中是否有环。
+package l02.n142;//给定一个链表，返回链表开始入环的第一个节点。 如果链表无环，则返回 null。
 //
 // 为了表示给定链表中的环，我们使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。 如果 pos 是 -1，则在该链表中没有环。 
+//
+// 说明：不允许修改给定的链表。 
 //
 // 
 //
 // 示例 1： 
 //
 // 输入：head = [3,2,0,-4], pos = 1
-//输出：true
+//输出：tail connects to node index 1
 //解释：链表中有一个环，其尾部连接到第二个节点。
 // 
 //
@@ -16,7 +18,7 @@ package l01.n141;//给定一个链表，判断链表中是否有环。
 // 示例 2： 
 //
 // 输入：head = [1,2], pos = 0
-//输出：true
+//输出：tail connects to node index 0
 //解释：链表中有一个环，其尾部连接到第一个节点。
 // 
 //
@@ -25,7 +27,7 @@ package l01.n141;//给定一个链表，判断链表中是否有环。
 // 示例 3： 
 //
 // 输入：head = [1], pos = -1
-//输出：false
+//输出：no cycle
 //解释：链表中没有环。
 // 
 //
@@ -34,8 +36,7 @@ package l01.n141;//给定一个链表，判断链表中是否有环。
 // 
 //
 // 进阶： 
-//
-// 你能用 O(1)（即，常量）内存解决此问题吗？ 
+//你是否可以不用额外空间解决此题？ 
 // Related Topics 链表 双指针
 
 
@@ -52,8 +53,8 @@ package l01.n141;//给定一个链表，判断链表中是否有环。
  * }
  * }
  */
-class Solution141 {
-    class ListNode {
+class Solution142 {
+    static class ListNode {
         int val;
         ListNode next;
 
@@ -63,20 +64,24 @@ class Solution141 {
         }
     }
 
-    public boolean hasCycle(ListNode head) {
-        if (head == null || head.next == null) {
-            return false;
-        }
+    public ListNode detectCycle(ListNode head) {
         ListNode fast = head;
         ListNode slow = head;
         while (fast != null && fast.next != null) {
             fast = fast.next.next;
             slow = slow.next;
-            if (slow == fast) {
-                return true;
+            if (fast.equals(slow)) {
+                fast = head;
+
+                while (fast != slow) {
+                    fast = fast.next;
+                    slow = slow.next;
+                }
+                return fast;
             }
         }
-        return true;
+        return null;
     }
+
 }
 //leetcode submit region end(Prohibit modification and deletion)
