4a76002d5707073c918f125f439d60c3323e3b85
Ran Zhang <zhangran42@126.com>
2020-04-15 17:18:32 +0800

    leetcode

--- src/leetcode/editor/cn/[122]买卖股票的最佳时机 II.java
+++ src/leetcode/editor/cn/[122]买卖股票的最佳时机 II.java
@@ -46,5 +46,33 @@ class Solution122 {
         }
         return max;
     }
+
+    /**
+     * 动态规划
+     *
+     * @param prices
+     * @return
+     */
+    public int maxProfit1(int[] prices) {
+        if (prices == null || prices.length == 0) {
+            return 0;
+        }
+        if (prices.length == 1) {
+            return 0;
+        }
+        // 定义dp[i][0]表示第i天的最大利润,0:持有现金，1：持有股票
+        int[][] dp = new int[prices.length][2];
+        dp[0][0] = 0;
+        dp[0][1] = -prices[0];
+        // 状态转移方程：
+        //第i天不持有，是前一天不持有，当天保持不变，或者是前一天持有，卖出去
+        //第i天持有，是前一天持有，当天保持不变，或者是前一天步持有，买进来
+        for (int i = 1; i < prices.length; i++) {
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
+            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] - prices[i]);
+        }
+        return dp[prices.length - 1][0];
+    }
+
 }
 //leetcode submit region end(Prohibit modification and deletion)
