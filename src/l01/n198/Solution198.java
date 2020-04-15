bce558d7419899dc5a4e3d12e9d769b4ae7384b1
zhangran <zhangran@42.com>
2020-04-15 12:43:33 +0800

    leetcode

--- src/l01/n198/Solution198.java
+++ src/l01/n198/Solution198.java
@@ -36,5 +36,23 @@ class Solution198 {
         }
         return Math.max(dp[nums.length - 1][0], dp[nums.length - 1][1]);
     }
+
+    public int rob1(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        if (nums.length == 1) {
+            return nums[0];
+        }
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+
+        for (int i = 2; i < nums.length; i++) {
+            dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i]);
+        }
+        return dp[nums.length - 1];
+
+    }
 }
 //leetcode submit region end(Prohibit modification and deletion)
