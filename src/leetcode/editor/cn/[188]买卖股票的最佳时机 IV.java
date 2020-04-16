e79265c77a84ea32af91a5cb6f44c7524d1b00ca
Ran Zhang <zhangran42@126.com>
2020-04-16 13:56:46 +0800

    leetcode

--- src/leetcode/editor/cn/[188]买卖股票的最佳时机 IV.java
+++ src/leetcode/editor/cn/[188]买卖股票的最佳时机 IV.java
@@ -1,4 +1,4 @@
-//给定一个数组，它的第 i 个元素是一支给定的股票在第 i 天的价格。 
+package leetcode.editor.cn;//给定一个数组，它的第 i 个元素是一支给定的股票在第 i 天的价格。
 //
 // 设计一个算法来计算你所能获取的最大利润。你最多可以完成 k 笔交易。 
 //
@@ -23,9 +23,40 @@
 
 
 //leetcode submit region begin(Prohibit modification and deletion)
-class Solution {
-    public int maxProfit(int k, int[] prices) {
+class Solution188 {
 
+    public int maxProfit(int k, int[] prices) {
+        int n = prices.length;
+        if (n == 0) {
+            return 0;
+        }
+        if (k > n / 2) {
+            return maxProfit1(prices);
+        }
+        int[][][] dp = new int[n][k + 1][2];
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j <= k; j++) {
+                if (i == 0) {
+                    dp[i][j][0] = 0;
+                    dp[i][j][1] = -prices[i];
+                    continue;
+                }
+                dp[i][j][0] = Math.max(dp[i - 1][j][0], dp[i - 1][j][1] + prices[i]);
+                dp[i][j][1] = Math.max(dp[i - 1][j][1], dp[i - 1][j - 1][0] - prices[i]);
+            }
+        }
+        return dp[n - 1][k][0];
+    }
+
+    private int maxProfit1(int[] prices) {
+        int max = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] > prices[i - 1]) {
+                max += prices[i] - prices[i - 1];
+            }
+        }
+        return max;
     }
 }
+
 //leetcode submit region end(Prohibit modification and deletion)
