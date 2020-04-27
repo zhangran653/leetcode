720ec185567c7801559df15987989dc13b9ba066
zhangran653 <zhangran42@126.com>
2020-04-27 16:17:57 +0800

    leetcode

---
+++ src/l02/n200/Solution200.java
@@ -0,0 +1,65 @@
+package l02.n200;//给定一个由 '1'（陆地）和 '0'（水）组成的的二维网格，计算岛屿的数量。一个岛被水包围，并且它是通过水平方向或垂直方向上相邻的陆地连接而成的。你可以假设
+//网格的四个边均被水包围。 
+//
+// 示例 1: 
+//
+// 输入:
+//11110
+//11010
+//11000
+//00000
+//
+//输出: 1
+// 
+//
+// 示例 2: 
+//
+// 输入:
+//11000
+//11000
+//00100
+//00011
+//
+//输出: 3
+// 
+// Related Topics 深度优先搜索 广度优先搜索 并查集
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution200 {
+
+    public int numIslands(char[][] grid) {
+        if (grid.length == 0) {
+            return 0;
+        }
+        int m = grid.length;
+        if (grid[0].length == 0) {
+            return 0;
+        }
+        int n = grid[0].length;
+        int count = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == '1') {
+                    count++;
+                    dfs(i, j, grid);
+                }
+            }
+        }
+        return count;
+    }
+
+    private void dfs(int i, int j, char[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        if (i < 0 || i >= m || j < 0 || j >= n||grid[i][j] != '1') {
+            return;
+        }
+        grid[i][j] = '0';
+        dfs(i + 1, j, grid);
+        dfs(i - 1, j, grid);
+        dfs(i, j + 1, grid);
+        dfs(i, j - 1, grid);
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
