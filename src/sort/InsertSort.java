8165c005c925a0e5a096828504b3e42bd4006d8a
zhangran653 <zhangran42@126.com>
2020-05-05 14:15:48 +0800

    sort

--- src/sort/InsertSort.java
+++ src/sort/InsertSort.java
@@ -9,26 +9,9 @@ import java.util.Arrays;
 public class InsertSort {
 
     /**
-     * 插入排序
-     *
-     * @param arr
-     */
-    public void sort(int[] arr) {
-        for (int i = 1; i < arr.length; i++) {
-            for (int j = i; j > 0; j--) {
-                if (arr[j] < arr[j - 1]) {
-                    int temp = arr[j - 1];
-                    arr[j - 1] = arr[j];
-                    arr[j] = temp;
-                } else {
-                    break;
-                }
-            }
-        }
-    }
-
-    /**
-     * 优化
+     * 它的工作原理是通过构建有序序列，对于未排序数据，在已排序序列中从后向前扫描，找到相应位置并插入
+     * 时间复杂度 |（平均）| （最坏） |（最好）| 空间复杂度 | 稳定性
+     * |         | O(n^2) |   O(n^2)| O(n)   | O(1)      | 稳定
      *
      * @param arr
      */
