d7d82aa2cd79ec12832a93b1a20ee1cc1272f2d0
zhangran653 <zhangran42@126.com>
2020-05-23 00:10:25 +0800

    leetcode

---
+++ src/leetcode/editor/cn/[338]比特位计数.java
@@ -0,0 +1,61 @@
+package leetcode.editor.cn;//给定一个非负整数 num。对于 0 ≤ i ≤ num 范围中的每个数字 i ，计算其二进制数中的 1 的数目并将它们作为数组返回。
+//
+// 示例 1: 
+//
+// 输入: 2
+//输出: [0,1,1] 
+//
+// 示例 2: 
+//
+// 输入: 5
+//输出: [0,1,1,2,1,2] 
+//
+// 进阶: 
+//
+// 
+// 给出时间复杂度为O(n*sizeof(integer))的解答非常容易。但你可以在线性时间O(n)内用一趟扫描做到吗？ 
+// 要求算法的空间复杂度为O(n)。 
+// 你能进一步完善解法吗？要求在C++或任何其他语言中不使用任何内置函数（如 C++ 中的 __builtin_popcount）来执行此操作。 
+// 
+// Related Topics 位运算 动态规划
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution338 {
+    public int[] countBits(int num) {
+        int[] res = new int[num + 1];
+        for (int i = 0; i <= num; i++) {
+            res[i] = count(i);
+        }
+        return res;
+    }
+
+    private int count(int i) {
+        int count = 0;
+        while (i != 0) {
+            i = i & (i - 1);
+            count++;
+        }
+        return count;
+    }
+
+    /**
+     * 动态规划 + 最后设置位【通过】
+     * 算法
+     * <p>
+     * 与上述方法思路相同，我们可以利用最后设置位。
+     * <p>
+     * 最后设置位是从右到左第一个为1的位。使用 x &= x - 1 将该位设置为0，就可以得到以下状态转移函数：
+     * <p>
+     * P(x)=P(x&(x−1))+1;
+     */
+    public int[] countBits2(int num) {
+        int[] res = new int[num + 1];
+        for (int i = 1; i <= num; i++) {
+            res[i] = res[i & (i - 1)] + 1;
+        }
+        return res;
+    }
+
+}
+//leetcode submit region end(Prohibit modification and deletion)
