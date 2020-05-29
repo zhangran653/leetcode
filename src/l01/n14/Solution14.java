16acf2e418cef6b80808e0aa72ec93e6a24ceba3
zhangran653 <zhangran42@126.com>
2020-05-29 18:16:43 +0800

    leetcode

---
+++ src/l01/n14/Solution14.java
@@ -0,0 +1,41 @@
+package l01.n14;//编写一个函数来查找字符串数组中的最长公共前缀。
+//
+// 如果不存在公共前缀，返回空字符串 ""。 
+//
+// 示例 1: 
+//
+// 输入: ["flower","flow","flight"]
+//输出: "fl"
+// 
+//
+// 示例 2: 
+//
+// 输入: ["dog","racecar","car"]
+//输出: ""
+//解释: 输入不存在公共前缀。
+// 
+//
+// 说明: 
+//
+// 所有输入只包含小写字母 a-z 。 
+// Related Topics 字符串
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution14 {
+    public String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+        for (int i = 0; i < strs[0].length(); i++) {
+            char c = strs[0].charAt(i);
+            for (int j = 0; j < strs.length; j++) {
+                if (i == strs[j].length() || c != strs[j].charAt(i)) {
+                    return strs[0].substring(0, i);
+                }
+            }
+        }
+        return strs[0];
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
